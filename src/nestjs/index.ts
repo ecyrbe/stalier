637bbea8b4b4856d5452ab33d406cbd5341b8ccd
ecyrbe <ecyrbe@gmail.com>
2022-08-01 01:53:07 +0200

    fix(nestjs): export module

--- src/nestjs/index.ts
+++ src/nestjs/index.ts
@@ -1,2 +1,5 @@
+export * from './stalier.constants';
 export * from './stalier.decorators';
 export * from './stalier.interceptor';
+export * from './stalier.interfaces';
+export * from './stalier.module';
